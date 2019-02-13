db62c643e751faccbea816256bc9854796247b51
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 18:24:20 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static#home'
-  resources :orders
-  resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
-  get '/products/:id/description', to: 'products#description'
-  get '/products/:id/inventory', to: 'products#inventory'
-  get '/products/:id/data', to: 'products#data'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
