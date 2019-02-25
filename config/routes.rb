70d0deeec3c0e2a213f66166fd574cd43cd17be5
maxwellbenton <maxwellbenton@gmail.com>
2019-02-25 15:23:14 -0500

    adds routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to: 'static#home'
+  resources :orders
+  resources :invoices
+  resources :products, only: [:show, :index, :new, :create]
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/data', to: 'products#data'
 end
